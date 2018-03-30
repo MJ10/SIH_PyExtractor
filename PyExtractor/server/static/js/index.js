943bc7d8a14e0402f9ab9f2ef928b2170c01a6d1
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2018-03-30 17:35:44 +0530

    index page basic ui added

--- PyExtractor/server/static/js/index.js
+++ PyExtractor/server/static/js/index.js
@@ -1,31 +1,18 @@
-var departments = ['Geology', 'Astromony', 'Physics'];
+$(document).ready(function() {
 
-$(document).ready(function (){
-    var optionsList = $('select');
-    // fill options
-    for (var i = 0; i < departments.length; ++i) {
-        $('select').append($("<option></option>").attr("value", i).text(departments[i]));
-    }
-    $('select').formSelect();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+      }); 
+    $('.carousel.carousel-slider').height($(document).height());  
 
+    setTimeout(function() {
+        autoplay();
+    }, 3000);   
 
-    $('#upload').click(function() {
-        if ($('select').val() == null || !$('input[type="file"]').val()) {
-                
-            M.toast({html: 'Please complete all fields'}, 1);            
-        }
+}); 
 
-        setTimeout(function() {
-            M.Toast.dismissAll();
-        }, 1000);
-
-        var formData = new FormData();
-        formData.append('file', $('input[type=file]')[0].files[0]);
-        console.log(formData);
-        console.log($('select').val());
-    });
-
-});
-
-
-
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
---
+++ PyExtractor/server/static/js/upload.js
@@ -0,0 +1,31 @@
+var departments = ['Geology', 'Astromony', 'Physics'];
+
+$(document).ready(function (){
+    var optionsList = $('select');
+    // fill options
+    for (var i = 0; i < departments.length; ++i) {
+        $('select').append($("<option></option>").attr("value", i).text(departments[i]));
+    }
+    $('select').formSelect();
+
+
+    $('#upload').click(function() {
+        if ($('select').val() == null || !$('input[type="file"]').val()) {
+                
+            M.toast({html: 'Please complete all fields'}, 1);            
+        }
+
+        setTimeout(function() {
+            M.Toast.dismissAll();
+        }, 1000);
+
+        var formData = new FormData();
+        formData.append('file', $('input[type=file]')[0].files[0]);
+        console.log(formData);
+        console.log($('select').val());
+    });
+
+});
+
+
+
