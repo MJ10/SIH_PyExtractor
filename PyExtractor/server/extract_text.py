405be3110460858b489bc2c58b17137a082599b5
Moksh Jain <mokshjn00@gmail.com>
2018-03-30 19:24:48 +0530

    add model

--- PyExtractor/server/extract_text.py
+++ PyExtractor/server/extract_text.py
@@ -1,11 +1,49 @@
 import os
+import subprocess
 import cv2
 import pytesseract
 import PIL
+import np
+
+SCRIPTS_PATH = os.path.join(os.getcwd(), 'scripts')
+
+def deskew(image_path):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.bitwise_not(gray)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    coords = np.column_stack(np.where(thresh > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    
+    # the `cv2.minAreaRect` function returns values in the
+    # range [-90, 0); as the rectangle rotates clockwise the
+    # returned angle trends to 0 -- in this special case we
+    # need to add 90 degrees to the angle
+    if angle < -45:
+        angle = -(90 + angle)
+    
+    # otherwise, just take the inverse of the angle to make
+    # it positive
+    else:
+        angle = -angle
+
+    # rotate the image to deskew it
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h),
+        flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    cv2.putText(rotated, "Angle: {:.2f} degrees".format(angle),
+        (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    
+    # show the output image
+    print("[INFO] angle: {:.3f}".format(angle))
+    cv2.imshow("Input", image)
+    cv2.imshow("Rotated", rotated)
 
 
-def preprocess(image):
-    pass
+def preprocess(image_path):
+    subprocess.run([os.path.join(SCRIPTS_PATH, 'textdeskew'), image_path, image_path])
 
 def detect_lang(image):
     pass
