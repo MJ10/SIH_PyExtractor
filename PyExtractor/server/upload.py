44609a999224e01ec95e9715460e0ba41f405851
Praveen Gupta <pvgupta24@gmail.com>
2018-03-30 11:39:09 +0530

    Location metadata module

--- PyExtractor/server/upload.py
+++ PyExtractor/server/upload.py
@@ -3,7 +3,9 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 import os.path
+
 from utilities.zip_extract import zip_extract
+from utilities.location import getGPS
 
 def upload(request):
 	return render(request, 'upload.html')
@@ -20,8 +22,12 @@ def upload(request):
     # Check if a file is sent
     if request.method == 'POST' and request.FILES['zip']:
         
+        # TODO : Wrap everything in a new process
+
         zip = request.FILES['zip']
         fs = FileSystemStorage()
+
+        # To make a subfolder of same name
         foldername = zip.name.split(".")[0]
 
         # a.zip will be stored in media/a/a.zip
@@ -29,9 +35,24 @@ def upload(request):
         path = os.path.join("media",file)
         print(file + " stored in " + path)
 
-        zip_extract(path,os.path.join("media",foldername))
+        # TODO : Extract images from uploaded zip
+        dest_path = os.path.join("media",foldername)
+        print("Extracted images in" + dest_path)
+        zip_extract(path, dest_path)
 
-		# TODO : Extract media/a/a.zip to 
+        # Stores list of metadata for uploaded images 
+        # TODO : extend to include other metadata
+        metadata = []
+
+        # TODO : Extract Metadata from images
+        for file in os.listdir(dest_path):            
+            # TODO : Change for other file types
+        	if(file.find('.jpg')):  
+        		gps = getGPS(os.path.join(dest_path,file))
+        		metadata.append(gps)
+        
+        print(metadata)
+
         return render(request, 'upload.html', {
             'uploaded_file_url': path
         })
